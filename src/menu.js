478f8fef00ff5642722d4a15e16133530c0994b9
mrjackmaster <jacektrg+github@gmail.com>
2021-05-21 00:53:00 +0200

    Added classes and libraries for foods. Not yet finished functions to generate those elements on page.

--- src/menu.js
+++ src/menu.js
@@ -1,13 +1,67 @@
-const showMenu = () => {
+// const showMenu = () => {
+//     const main = document.createElement('div');
+//     main.classList.add('main');
+// }
+
+const menu = document.getElementById('mainContainer');
+
+const pizzaMenu = () => {
     const main = document.createElement('div');
-    main.classList.add('main');
-    
+        pizzaList.forEach((pizza) => {
+            
+        })
+    return main;
 }
 
-const loadMenuPage = () => {
-    const menu = document.getElementById('mainContainer');
-    menu.innerHTML = '';
-    menu.appendChild(showMenu());
+const drinksMenu = () => {
+    const main = document.createElement('div');
+        drinksList.forEach((drink) => {
+
+        })
+    return main;
 }
 
-export default loadMenuPage;
+const emptyMenu = () => {
+    const main = document.createElement('div');
+        emptyList.forEach((empty) => {
+
+        })
+    return main;
+}
+
+const pizzaList = [];
+const drinksList = [];
+const emptyList = [];
+
+class newPizza {
+    constructor(name, description) {
+        this.name = name;
+        this.description = description;
+    }
+}
+
+class newDrink {
+    constructor(name, description) {
+        this.name = name;
+        this.description = description;
+    }
+}
+
+class newEmpty {
+    constructor(name, description) {
+        this.name = name;
+        this.description = description;
+    }
+}
+
+// const loadMenuPage = () => {
+//     const menu = document.getElementById('mainContainer');
+//     menu.innerHTML = '';
+//     menu.appendChild(showMenu());
+// }
+
+export {
+    pizzaMenu,
+    drinksMenu,
+    emptyMenu,
+}
