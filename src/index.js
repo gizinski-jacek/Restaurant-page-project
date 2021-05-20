738571f84faa61c69f14bca5a3ee0b45ebc229fd
mrjackmaster <jacektrg+github@gmail.com>
2021-05-21 00:58:35 +0200

    Added dropdown menu. Also unfinished func to hide that menu on a click. Might also have to rename some symbols for clarity.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,29 @@
+import * as menu from './menu'
 import loadHomePage from './home'
-import loadMenuPage from './menu'
 import loadContactPage from './contact'
 
+
+
 const createHeader = () => {
+    const main = document.createElement('div');
+    main.classList.add('header');
+
     const header1 = document.createElement('h1');
-    header1.classList.add('header');
     header1.textContent = 'Pizzaliano';
 
     const header2 = document.createElement('h2');
-    header2.textContent = 'The Best Place For Your Pizza Needs';
-    header1.appendChild(header2);
+    header2.setAttribute('style', 'white-space: pre;');
+    header2.textContent = 'The Best Place \nFor Your Pizza Needs';
+    
+    main.appendChild(header1);
+    main.appendChild(header2);
 
-    return header1;
+    return main;
 };
 
 const createNavBar = () => {
     const navBar = document.createElement('div');
+    navBar.id = 'navBar';
 
     const homeBtn = document.createElement('button');
     homeBtn.classList.add('homeBtn');
@@ -25,12 +33,47 @@ const createNavBar = () => {
         loadHomePage();
     })
 
+    const menuContainer = document.createElement('div');
+    menuContainer.classList.add('menuContainer');
+
     const menuBtn = document.createElement('button');
     menuBtn.classList.add('menuBtn');
-    menuBtn.setAttribute('type', 'button');
     menuBtn.textContent = 'Menu';
-    menuBtn.addEventListener('click', () => {
-        loadMenuPage();
+    menuBtn.addEventListener('click', funcDropdown);
+
+    const dropdownContainer = document.createElement('div');
+    dropdownContainer.id = 'dropCont';
+    dropdownContainer.classList.add('dropCont');
+
+    const pizza = document.createElement('button');
+    pizza.id = 'pizza';
+    pizza.textContent = 'Pizza';
+    pizza.addEventListener('click', () => {
+        menu.pizzaMenu();
+    })
+
+    const drinks = document.createElement('button');
+    drinks.id = 'drinks';
+    drinks.textContent = 'Drinks';
+    drinks.addEventListener('click', () => {
+        menu.drinksMenu();
+    })
+
+    const empty = document.createElement('button');
+    empty.id = 'emptyMenu';
+    empty.textContent = 'Empty';
+    empty.addEventListener('click', () => {
+        menu.emptyMenu();
+    })
+
+    dropdownContainer.appendChild(pizza);
+    dropdownContainer.appendChild(drinks);
+    dropdownContainer.appendChild(empty);
+    menuContainer.appendChild(menuBtn);
+    menuContainer.appendChild(dropdownContainer);
+
+    homeBtn.addEventListener('click', () => {
+        loadHomePage();
     })
 
     const contactBtn = document.createElement('button');
@@ -42,7 +85,7 @@ const createNavBar = () => {
     })
 
     navBar.appendChild(homeBtn);
-    navBar.appendChild(menuBtn);
+    navBar.appendChild(menuContainer);
     navBar.appendChild(contactBtn);
 
     return navBar;
@@ -63,6 +106,26 @@ const createFooter = () => {
 
     return footer;
 }
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+const funcDropdown = () => {
+    document.getElementById('dropCont').classList.toggle('show');
+}
+
+// Close the dropdown if the user clicks outside of it
+// window.onclick = (e) => {
+//     if (!e.target.matches('.menuBtn')) {
+//         let dropdownsList = document.getElementById('dropCont');
+//         let i;
+//         for (i = 0; i < dropdownsList.length; i++) {
+//             let openDropdown = dropdownsList[i];
+//             if (openDropdown.classList.contains('show')) {
+//                 openDropdown.classList.remove('show');
+//             }
+//         }
+//     }
+// }
 
 const loadSite = (() => {
     const content = document.getElementById('content');
